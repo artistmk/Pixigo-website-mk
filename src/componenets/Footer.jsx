f67a847132a61dbedfab0b9eba78283ae58b5f38
artistmk <firewithmk172000@gmail.com>
2025-05-12 16:05:13 +0530

    donee1

--- src/componenets/Footer.jsx
+++ src/componenets/Footer.jsx
@@ -1,52 +1,51 @@
-import React from 'react'
 import '../responsive/Footer.css'
 
 const Footer = () => {
- 
   return (
     <>
-    <footer>
-      <div className='foot-panel3'></div>
+      <footer>
+        <div className="panel2">
+          <div className="footer-content">
+            <ul>
+              <p>Terms & Condition</p>
+              <a href="#">About Us</a>
+              <a href="#">Carrers</a>
+              <a href="#">Articles</a>
+              <a href="#">Our Terms</a>
+            </ul>
 
-      <div className="foot-panel2">
-          <ul>
-             <p>Terms & Condition</p>
-             <a>About Us</a>
-             <a>Careers</a>
-             <a>Articles</a>
-             <a>Our Terms</a>
-          </ul>
-          <ul>
-            <p>Connect with Us</p>   
-            <a>Facebook</a>
-            <a>Twitter</a>
-            <a>Instagram</a>
-            <a>Youtube</a>
-         </ul>
-         <ul>
-          <p>Upload Your Images</p>               	
-          <a>See on Pixigo</a>
-          <a>Protect and Build Your Brand</a>
-          <a>Pixigo Offering</a>
-          <a>Advertise Your Images</a>
-       </ul>
-       <ul>
-        <p>Help & Support</p>                  
-        <a>Your Account</a>
-        <a>100% Protection</a>
-        <a>Recalls and Product Safety Alerts</a>
-        <a>Help</a>
-     </ul>
-      </div>
-      
-      <div className="foot-panel4">
-        <div className="pages">
-        <p className="text-center">Copyright &copy; PIXIGO.com</p>
+            <ul>
+              <p>Connect With Us</p>
+              <a href="#">Facebook</a>
+              <a href="#">Instagram</a>
+              <a href="#">Twitter</a>
+              <a href="#">Youtube</a>
+            </ul>
+
+            <ul>
+            <p>Upload Your Images</p>               	
+            <a href='#'>See on Pixigo</a>
+            <a href='#'>Protect and Build Your Brand</a>
+            <a href='#'>Pixigo Offering</a>
+            <a href='#'>Advertise Your Images</a>
+            </ul>
+
+            <ul>
+           <p>Help & Support</p>                  
+           <a href='#'>Your Account</a>
+           <a href='#'>100% Protection</a>
+           <a href='#'>Recalls and Product Safety Alerts</a>
+           <a href='#'>Help</a>
+            </ul>
+          </div>
         </div>
-      </div>
-     </footer>
-    </>
-  )
-}
 
-export default Footer
+        <div className="panel3">
+          <p className="text-center">Copyright &copy; PIXIGO.com</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
